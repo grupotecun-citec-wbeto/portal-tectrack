48afa47f1e5a8ca6aa3d02ffc8c6179c36d742e8
Humberto Antonio Herrador Reyes <47198640+wbeto@users.noreply.github.com>
2024-10-21 17:05:54 -0600

    pre diagnostico

--- src/appContext.js
+++ src/appContext.js
@@ -1,25 +1,54 @@
 // AppContext.js
-import { createContext, useState } from 'react';
-
+import { createContext, useState, useEffect } from 'react';
+// AXIOS
+import axios from 'axios';
 // Enums
 import Enums from './Enums';
 
 // Crear el contexto
 const AppContext = createContext();
 
+
 // Crear el proveedor del contexto
 export function AppProvider({ children }) {
+    
+     
     // ACTIVE INDEX
     const [sideBarAccordionActiveIndex, setSideBarAccordionActiveIndex] = useState(null);
     const [machineID,setMachineID] = useState(null)
     const [caseType,setCaseType] = useState(Enums.CORRECTIVO) // CORRECTIVO, PREVENTIVO
     const [comunicationSelected,setComunicationSelected] = useState(Enums.WHATSAPP)
+    const [serviceTypeData,setServiceTypeData] = useState(null)
+
+    useEffect(async() => {
+      
+        //onSearch(debouncedSearchValue);
+        setServiceTypeData([])
+        const fetchData = async () => {
+          try {
+            const response = await axios.get(`http://localhost:5000/api/v1/servicesType`);
+            
+            let data = JSON.parse(response.data)
+        
+
+            
+            setServiceTypeData(data);
+          } catch (error) {
+            setServiceTypeData([])
+            console.error('Error al obtener datos:', error);
+            
+          }
+        };
+        fetchData();
+      
+    }, []);
 
     return (
         <AppContext.Provider value={{ 
             sideBarAccordionActiveIndex, setSideBarAccordionActiveIndex,
             machineID, setMachineID,
-            caseType,setCaseType
+            caseType,setCaseType,
+            serviceTypeData,setServiceTypeData
             }}>
             {children}
         </AppContext.Provider>
--- src/components/PreDiagnostico/CheckboxPreDiagnostico.js
+++ src/components/PreDiagnostico/CheckboxPreDiagnostico.js
@@ -1,3 +1,4 @@
+import React,{useState,useContext} from 'react';
 import {
     Input,
     InputGroup,
@@ -12,23 +13,58 @@ import {
     Heading,
     Image,
     Button,
+    Select,
   } from '@chakra-ui/react';
+
+  import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+  } from '@chakra-ui/react'
+import { column } from 'stylis';
+
+import AppContext from 'appContext';
+
 
 
 function CheckboxPreDiagnostico(props){
     const {name,...rest} = props
 
+    const [check,setCheck] = useState(false)
+
+    const {serviceTypeData,setServiceTypeData} = useContext(AppContext)
+
     return(
-      <Flex align='center' mb='20px'>
-        <Switch colorScheme='blue' me='10px' />
-        <Text
-          noOfLines={1}
-          fontSize='md'
-          color='gray.400'
-          fontWeight='400'>
-          {name}
-        </Text>
+      <>
+      <Flex align={{sm:'left',xl:'left'}} direction={{sm:'column'}} mb='20px'>
+        <Flex>
+          <Switch colorScheme='blue' me='10px' onChange={() => setCheck(!check)} />
+          <Text
+            noOfLines={1}
+            fontSize='md'
+            color='gray.400'
+            fontWeight='400'>
+            {name}
+          </Text>
+        </Flex>
+        { check && (
+          <Flex ms={{xl:'10px'}}>
+            <FormControl maxW={{xl:'250px'}}>
+              <FormLabel htmlFor='country'>Tipo de servicio</FormLabel>
+              <Select id='country' placeholder='Selecconar servicio' onChange={(event) => alert(event.target.value)}>
+                {serviceTypeData.map( (data) =>(
+                  <option value={data.ID}>{data.servicio_tipo_name}</option>
+                ))}
+              </Select>
+            </FormControl>
+            
+          </Flex>
+        )}
+       
       </Flex>
+      
+      </>
     )
    
 }
