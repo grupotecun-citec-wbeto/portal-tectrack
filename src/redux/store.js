ab3820a37977a4d27af986d21650744f707e95c8
Humberto Antonio Herrador Reyes <47198640+wbeto@users.noreply.github.com>
2024-10-24 09:14:22 -0600

    Se agrego el uso de indexdb-storage para redux-presist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,22 @@
 // store.js
 import { createStore } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // Usaremos localStorage
+//import storage from 'redux-persist/lib/storage'; // Usaremos localStorage
+import indexedDBStorage from 'redux-persist-indexeddb-storage'; // Adaptador para IndexedDB
 import caseReducer from './caseReducer';
 
-// Configuración de persistencia
+
+// Configuración de Redux-Persist
 const persistConfig = {
   key: 'root',
-  storage,  // Usar localStorage
+  storage: indexedDBStorage('myAppDB'), // Aquí defines tu IndexedDB
 };
+
+// Configuración de persistencia
+/*const persistConfig = {
+  key: 'root',
+  storage,  // Usar localStorage
+};*/
 
 // Crear el reducer persistente
 const persistedReducer = persistReducer(persistConfig, caseReducer);
