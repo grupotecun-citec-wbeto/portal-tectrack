020bf031d281ec7226b7086f49420ac9d9bed5c4
Humberto Antonio Herrador Reyes <47198640+wbeto@users.noreply.github.com>
2025-04-21 11:13:56 -0600

    Limpieza de Cargar Caso

--- src/hookDB/cargarCaso.js
+++ src/hookDB/cargarCaso.js
@@ -1,12 +1,11 @@
 import { useState,useContext,useEffect } from 'react';
-import SqlContext from 'sqlContext';
-import axios from 'axios';
+
 //redux
 import { useSelector, useDispatch } from 'react-redux';
-import { use } from 'react';
 
 import { useDataBaseContext } from 'dataBaseContext';
 
+// repositorios de datos
 import syncRepository from 'repositories/api/syncRepository';
 import repositoryDiagnostico from 'repositories/local/diagnostico/repository';
 import repositoryVisita from 'repositories/local/visita/repository';
@@ -16,7 +15,7 @@ import repositoryCaso from 'repositories/local/caso/repository';
 
 function useCargarCaso(caso_id) {
   
-  
+  // Preparar la base de datos
   const {dbReady} = useDataBaseContext()
 
     // *********************************** HOOK CASO **************************************
@@ -40,39 +39,7 @@ function useCargarCaso(caso_id) {
     /*====================FIN BLOQUE: REDUX-PERSIST ==============*/
 
 
-  const[startLoad,setStartLoad] = useState(false)
-
-  
-  const [formData, setFormData] = useState([]);
   const [times,setTimes] = useState({'caso':300000})
-  const [dataCasoSync,setDataCasoSync] = useState({})
-  const [formDatadiagnosticos,setFormDataDiagnosticos] = useState({})
-  const [formDataProgramas,setFormDataProgramas] = useState({})
-  const [formDataVisitas,setFormDataVisitas] = useState({})
-  const [formDataCasoVisitas,setFormDataCasoVisitas] = useState({})
-  const [listaCasos,setListaCasos] = useState({})
-
-  const [formDataMerge,setFormDataMerge] = useState({})
-
-
-  // prueba
-  /*useEffect(() => {
-    const fetchData = async () => {
-      const data = await repositoryDiagnostico.findByListCaseIds(['144fa9e9-3cb4-4aa5-9552-14dc15c616c3','24cd5fc3-65b9-4e2c-b8d4-099330a26f08'])
-      console.log('fetchData: a5ae9870-6c39-49a6-ae0c-7314dad60a1c', data);
-    }
-    fetchData()
-  },[caso_id])*/
-
-  /*useEffect( () => {
-    console.log('loadCaso rehidratarDb:', '8230d797-8360-4920-85fa-1d359221ac12');
-    rehidratarDb()
-  },[])*/
-
-  // Rehidratar la base de dato
-  /*useEffect( () =>{
-    if(!db) rehidratarDb()
-  },[db,rehidratarDb])*/
 
   /**
    * Obtener lista de casos no sincronizados
@@ -92,13 +59,10 @@ function useCargarCaso(caso_id) {
           if(casosNoSincronizados.length != 0){  
             console.log('loadCaso e91d3519-15d5-4d90-aa0d-1ae507eb6943', casosNoSincronizados); 
             const uuids = casosNoSincronizados.map(objeto => objeto.ID);
-            /*setListaCasos((prev) => ({
-              ...prev,
-              casosNoSincronizados: casosNoSincronizados,
-              uuids: uuids
-            }));*/
-            const formDataMerge2 = await getDataMerge({casosNoSincronizados: casosNoSincronizados,uuids: uuids})
-            await syncCloud(formDataMerge2)
+            
+            const listaCasos = {casosNoSincronizados: casosNoSincronizados,uuids: uuids}
+            const formDataMerge = await getDataMerge(listaCasos)
+            await syncCloud(formDataMerge)
           }else{
             console.log('loadCaso fail 699d355d-15f8-4c54-b900-cad9c08b67a9', casosNoSincronizados);
             setListaCasos({})
@@ -147,7 +111,6 @@ function useCargarCaso(caso_id) {
           try{  
             
             const data = await syncRepository.syncLocalCasesWithCloud(formDataMerge)
-            console.log(data,'3b369c1c-cbb1-4aff-9f2f-b35db259937c')
             setDataCasoSync(data)
             for (const uuid of Object.keys(data)) {
               repositoryCaso.setAsSynchronized(uuid);
