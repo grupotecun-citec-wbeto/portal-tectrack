f0d61320a2a6a478d4aa89c16bb0a9f256a32d8f
Humberto Antonio Herrador Reyes <47198640+wbeto@users.noreply.github.com>
2025-11-29 15:15:03 -0600

    Agregar mensaje de equipo imcompleto antes de terminar el caso

--- src/components/Diagnostico/CardTerminarCaso.js
+++ src/components/Diagnostico/CardTerminarCaso.js
@@ -17,6 +17,15 @@ import {
     Select,
     Button,
     Grid,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure,
+    Icon
 } from '@chakra-ui/react';
 // formularios
 import {
@@ -25,6 +34,8 @@ import {
     FormErrorMessage,
     FormHelperText,
 } from '@chakra-ui/react'
+
+import { FaExclamationTriangle } from "react-icons/fa";
 
 
 // Custom components
@@ -68,6 +79,8 @@ const CardTerminarCaso = forwardRef((props, ref) => {
     const history = useHistory()
 
     const [caseId, setCaseId] = useState(0)
+
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
 
     /*=======================================================
@@ -121,13 +134,18 @@ const CardTerminarCaso = forwardRef((props, ref) => {
             const equipos_keys = Object.keys(equipos)
 
             const rest = equipos_keys.reduce((acc, maquina_id) => {
-                if (acc == false)
-                    return false
-                if (userData.casos[userData.casoActivo?.code].equipos[maquina_id].diagnostico.sistemasSelectedJson.length == 0
-                    || Object.keys(userData.casos[userData.casoActivo?.code].equipos[maquina_id].diagnostico.herramientas) == 0) {
-                    return false
+                if (acc === false) return false;
+
+                const equipo = userData.casos[userData.casoActivo?.code]?.equipos[maquina_id];
+                const diagnostico = equipo?.diagnostico || {};
+                const sistemas = diagnostico.sistemasSelectedJson || [];
+                const herramientas = diagnostico.herramientas || {};
+
+                // Validar que haya sistemas seleccionados y herramientas registradas
+                if (sistemas.length === 0 || Object.keys(herramientas).length === 0) {
+                    return false;
                 }
-                return true
+                return true;
             }, true)
 
             return rest
@@ -188,10 +206,12 @@ const CardTerminarCaso = forwardRef((props, ref) => {
 
                     //history.push('/admin/pages/casos')
                 } else {
-                    alert('Elegir el caso')
+                    // alert('Elegir el caso')
+                    onOpen();
                 }
             } else {
-                alert('Ingresar todos los diagnosticos')
+                // alert('Ingresar todos los diagnosticos')
+                onOpen();
             }
 
         } catch (err) {
@@ -204,17 +224,50 @@ const CardTerminarCaso = forwardRef((props, ref) => {
     }));
 
     return (
-        <Card>
-            <CardHeader>
-                <Heading size='md'>Finalizar Caso</Heading>
-            </CardHeader>
-            <CardBody>
-                <Text fontSize='lg' color='gray.500' fontWeight='bold'>
-                    Listo para terminar el caso. Haga clic en el botón flotante para confirmar.
-                </Text>
-            </CardBody>
+        <>
+            <Card>
+                <CardHeader>
+                    <Heading size='md'>Finalizar Caso</Heading>
+                </CardHeader>
+                <CardBody>
+                    <Text fontSize='lg' color='gray.500' fontWeight='bold'>
+                        Listo para terminar el caso. Haga clic en el botón flotante para confirmar.
+                    </Text>
+                </CardBody>
 
-        </Card>
+            </Card>
+
+            <Modal isOpen={isOpen} onClose={onClose} isCentered size="lg">
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader display="flex" alignItems="center" color="red.500">
+                        <Icon as={FaExclamationTriangle} mr={3} w={6} h={6} />
+                        Diagnósticos Incompletos
+                    </ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <Text fontSize="md" mb={4} fontWeight="bold">
+                            No se puede terminar el caso porque faltan diagnósticos.
+                        </Text>
+                        <Text mb={2}>
+                            Asegúrese de que todos los equipos tengan:
+                        </Text>
+                        <Flex direction="column" ml={4} mb={4} as="ul" style={{ listStyleType: 'disc' }}>
+                            <Text as="li">Al menos un <b>sistema</b> seleccionado.</Text>
+                            <Text as="li">Las <b>herramientas</b> utilizadas registradas.</Text>
+                        </Flex>
+                        <Text fontSize="sm" color="gray.500">
+                            Revise cada equipo y complete la información requerida antes de finalizar.
+                        </Text>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button colorScheme="red" onClick={onClose}>
+                            Entendido
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
     )
 });
 
