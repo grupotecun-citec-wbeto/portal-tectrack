d8e52ef410e91eadd13115c448869bfc0e01b5dd
Humberto Antonio Herrador Reyes <47198640+wbeto@users.noreply.github.com>
2025-12-01 11:05:43 -0600

    Agregar proyecto a rutas dinamicas

--- src/features/administrative/AdministrativeRoute.js
+++ src/features/administrative/AdministrativeRoute.js
@@ -7,7 +7,7 @@ import {PersonIcon} from "components/Icons/Icons";
 
 
 /** @type Feature */
-export const AdministrativeFeature = {
+export const CapacitationFeature = {
   enabled: true,
   category: "",
   order: 1.2,
@@ -22,7 +22,27 @@ export const AdministrativeFeature = {
       layout: "/admin",
     },
 };
+/** @type Feature */
+export const ProjectFeature = {
+  enabled: true,
+  category: "",
+  order: 1.2,
+  route:  {
+    path: "/pages/programa/proyecto",
+    name: "Proyecto",
+    rtlName: "لوحة القيادة",
+    icon: <PersonIcon color='inherit' />,
+    secondaryNavbar: true,
+    visible: false,
+    component: AdministrativeDash,
+    layout: "/admin",
+  },
+};
 
+
+/**
+ *  ,
+ */
 /*
 NOTA: Si se quiere crear una categoria se agregar texto en category
  */
--- src/features/featuresService.js
+++ src/features/featuresService.js
@@ -2,12 +2,12 @@ import { SegmentsFeature } from "./segments";
 import { CasesFeature } from "./cases";
 import { SearchBoxFeature } from "./searchbox";
 import { PreDiagnosticoFeature } from "./prediagnostico";
-import { AdministrativeFeature } from "./administrative";
+import { CapacitationFeature } from "./administrative";
 
 export const FEATURES = [
   SegmentsFeature,
   CasesFeature,
   SearchBoxFeature,
   PreDiagnosticoFeature,
-  AdministrativeFeature
+  CapacitationFeature
 ];
